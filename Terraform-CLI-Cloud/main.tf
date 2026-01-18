8000ba280c00f660a6896afe30516040c526b191
inchara1996 <incharanbhushan1996@gmail.com>
2026-01-18 12:39:20 +0530

    Update main.tf
    changing hello good evening to hello good afternoon

--- Terraform-CLI-Cloud/main.tf
+++ Terraform-CLI-Cloud/main.tf
@@ -10,9 +10,10 @@ variable "v1" {
 
 variable "v2" {
   type    = string
-  default = "hello good evening"
+  default = "hello good afternoon"
 }
 
 output "o1" {
   value = local_file.inchara.id
+
 }
